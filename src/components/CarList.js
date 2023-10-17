af6040d47048e4930fe075bb44aeac1f6531e540
zaktech96 <zakariyesahid96@gmail.com>
2023-10-17 15:17:53 +0100

    project complete

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,17 +1,26 @@
 import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
+
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (car) => {
     dispatch(removeCar(car.id));
   };
+
   const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
@@ -25,6 +34,7 @@ function CarList() {
       </div>
     );
   });
+
   return (
     <div className="car-list">
       {renderedCars}
